77f925b5ebd0d2172a4774946e3d17e32a2192ba
vini <vineethyadavnaluvala@gmail.com>
2023-05-29 01:31:43 +0530

    first commit

---
+++ src/main/java/in/vini/SbmsWelcomeApiConfigServerClientApplication.java
@@ -0,0 +1,13 @@
+package in.vini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsWelcomeApiConfigServerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsWelcomeApiConfigServerClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/in/vini/rest/WelcomeRestController.java
@@ -0,0 +1,17 @@
+package in.vini.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+
+	@Value("${msg}")
+	private String msg;
+	
+	@GetMapping("/welcome")
+	public String welcomeMsg() {
+		return msg;
+	}
+}
